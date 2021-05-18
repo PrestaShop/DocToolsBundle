f263eb7784881a4571cecf195c28f70816ba63f4
Jonathan Lelievre <jonathan.lelievre@prestashop.com>
2021-05-18 18:46:34 +0200

    Move all code into src folder

--- src/DependencyInjection/DocToolsExtension.php
+++ src/DependencyInjection/DocToolsExtension.php
@@ -41,7 +41,7 @@ class DocToolsExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
 
         $this->processConfiguration(new Configuration(), $configs);
